c976d48b948b149c402b566a15f67f4c95793f78
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-03-16 20:15:39 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let tInput = document.getElementById("tempInput");
+let cRadio = document.getElementById("radioC");
+let fRadio = document.getElementById("radioF");
+
+
+document.getElementById("convertBtn").addEventListener("click", determineConverter);
+document.getElementById(radioBtn).addEventListener
+
+function determineConverter() {
+    console.log(tInput.value);
+}

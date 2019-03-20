901a45f0176ec7ba02124ada47db1784a82a0dc2
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-03-19 18:02:29 -0600

    clear button working!

--- main.js
+++ main.js
@@ -8,46 +8,46 @@
 // }
 
 document.getElementById("convertBtn").addEventListener("click", determineConverter);
-var output = document.getElementById("output");
-var clearButton = document.getElementById("clear");
-var button = document.getElementById("convertBtn")
+const output = document.getElementById("tempOutput");
+const clearButton = document.getElementById("clearBtn");
+const button = document.getElementById("convertBtn");
+const temp = tempInput.value;
 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+}
 
 // T(°C) = (T(°F) - 32) × 5/9
-function toCelsius (fValue) {
-    var cValue = (fValue - 32) * (5/9);
-    output.innerHTML = cValue;
-    colorOutput("c", cValue);
-    return cValue;
+function toCelsius (temp) {
+    const finalTemp = (temp - 32) * (5/9);
+    output.innerHTML = finalTemp;
+    // colorOutput("c", cValue);
 }
 
 // T(°F) = T(°C) × 9/5 + 32
-function toFahrenheit (cValue) {
-    var fValue = (cValue * (9/5)) + 32;
-    output.innerHTML = fValue;
-    colorOutput("f", fValue);
-    return fValue;
+function toFahrenheit (temp) {
+    const finalTemp = (temp * (9/5)) + 32;
+    output.innerHTML = finalTemp;
+    // colorOutput("f", fValue);
 }
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
 
 // Check output to apply coloring.
-function colorOutput (type, value) {
-    if ((type === "c" && value > 32) || (type === "f" && value > 90)) {
-        output.style.color = "red";
-    } else if ((type === "c" && value < 0) || (type === "f" && value < 32)) {
-        output.style.color = "blue";
-    } else {
-        output.style.color = "green";
-    }
-}
+// function colorOutput (type, value) {
+//     if ((type === "c" && value > 32) || (type === "f" && value > 90)) {
+//         output.style.color = "red";
+//     } else if ((type === "c" && value < 0) || (type === "f" && value < 32)) {
+//         output.style.color = "blue";
+//     } else {
+//         output.style.color = "green";
+//     }
+// }
 
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-    var toConvert = document.getElementById("toConvert").value;
+    const toConvert = document.getElementById("tempInput").value;
     if (document.getElementById("F").checked) {
         toFahrenheit(toConvert);
     } else if (document.getElementById("C").checked) {
@@ -58,14 +58,14 @@ function determineConverter (clickEvent) {
 }
 
 function clear() {
- document.getElementById("F").checked = false;
- document.getElementById("C").checked = false;
+//  document.getElementById("F").checked = false;
+//  document.getElementById("C").checked = false;
  output.innerHTML = "";
- document.getElementById("toConvert").value = "";
+ document.getElementById("tempInput").value = "";
 }
 
 
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
-document.getElementById("clear").addEventListener("click", clear);
+clearButton.addEventListener("click", clear);
